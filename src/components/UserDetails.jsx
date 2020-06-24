46360d1bd4aadb165aef6b372f57be3e00af4550
nax2uk <azlinayeo@gmail.com>
2020-06-24 22:45:25 +0100

    formatting

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -4,7 +4,7 @@ const UserDetails = (props) => {
   const { user, setUser } = props;
   return (
     <span className="badge badge-pill bg-light align-text-bottom">
-      <img alt={user.username} src={user.avatar_url} className="border border-dark mr-2 mt-1" /> {user.username} <span className="divider ml-2"> | </span>
+      <img alt={user.username} src={user.avatar_url} className="border border-dark mr-1 mt-1" /> {user.username} <span className="divider ml-1"> | </span>
       <button className="btn btn-light ml-0 my-0 text-primary" onClick={() => setUser({ username: "", avatar_url: "", name: "" })}>Log out </button></span>
   );
 };
